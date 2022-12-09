c0098c44002b32652f59fa3d4b0a1600148f045e
Everett Southwick <everett@southwick.dev>
2022-12-09 00:39:05 -0500

    update newtonsoft

--- SongAcronymBot.Api/Program.cs
+++ SongAcronymBot.Api/Program.cs
@@ -4,7 +4,6 @@ using SongAcronymBot.Domain.Repositories;
 using SongAcronymBot.Domain.Services;
 
 var builder = WebApplication.CreateBuilder(args);
-
 builder.Services.AddDbContext<SongAcronymBotContext>
     (options =>
         options.UseSqlServer(builder.Configuration.GetConnectionString("Production"))
@@ -15,7 +14,7 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddTransient(typeof(IRepository<>), typeof(Repository<>));
 builder.Services.AddTransient<IAcronymRepository, AcronymRepository>();
-builder.Services.AddTransient<IRedditorRepository,RedditorRepository>();
+builder.Services.AddTransient<IRedditorRepository, RedditorRepository>();
 builder.Services.AddTransient<ISubredditRepository, SubredditRepository>();
 builder.Services.AddTransient<ISpotifyService, SpotifyService>();
 
