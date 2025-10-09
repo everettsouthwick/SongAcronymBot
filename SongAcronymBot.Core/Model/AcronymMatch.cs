9f598487628f60fb905e414473e7d716dc52d6aa
Everett Southwick <everett@southwick.dev>
2025-10-09 16:20:32 -0400

    Refactor RedditServiceTests and improve Acronym comments
    Removed mockSubredditRepository from RedditServiceTests.
    Updated AcronymMatch to clarify comments for tracks that are also albums by checking if TrackName matches AlbumName.

--- SongAcronymBot.Core/Model/AcronymMatch.cs
+++ SongAcronymBot.Core/Model/AcronymMatch.cs
@@ -22,7 +22,9 @@ namespace SongAcronymBot.Core.Model
                 AcronymType.Album => $"- {acronym.AcronymName} could mean *{acronym.AlbumName}* ({acronym.YearReleased}), an album by {acronym.ArtistName}.\n",
                 AcronymType.Artist => $"- {acronym.AcronymName} could mean {acronym.ArtistName}.\n",
                 AcronymType.Single => $"- {acronym.AcronymName} could mean \"{acronym.TrackName}\", a single by {acronym.ArtistName}.\n",
-                AcronymType.Track => $"- {acronym.AcronymName} could mean \"{acronym.TrackName}\", a track from *{acronym.AlbumName}* ({acronym.YearReleased}) by {acronym.ArtistName}.\n",
+                AcronymType.Track => acronym.TrackName == acronym.AlbumName
+                    ? $"- {acronym.AcronymName} could mean \"{acronym.TrackName}\" (track) or *{acronym.AlbumName}* (album) ({acronym.YearReleased}) by {acronym.ArtistName}.\n"
+                    : $"- {acronym.AcronymName} could mean \"{acronym.TrackName}\", a track from *{acronym.AlbumName}* ({acronym.YearReleased}) by {acronym.ArtistName}.\n",
                 _ => $"- {acronym.AcronymName} could mean {acronym.TrackName}, a track from *{acronym.AlbumName}* ({acronym.YearReleased}) by {acronym.ArtistName}.\n",
             };
             Position = index;
