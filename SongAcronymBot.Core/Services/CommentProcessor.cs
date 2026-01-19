3add82db773c4613fbfa646f5e1c6f76652103fd
Everett Southwick <everett@southwick.dev>
2026-01-19 13:23:34 -0500

    feat: Add CommentProcessor service to handle Reddit comment processing, acronym matching, and user opt-in/out.

--- SongAcronymBot.Core/Services/CommentProcessor.cs
+++ SongAcronymBot.Core/Services/CommentProcessor.cs
@@ -55,11 +55,11 @@ namespace SongAcronymBot.Core.Services
             }
             catch (RedditForbiddenException ex)
             {
-                _logger.LogWarning(ex, "Failed to reply - forbidden", ex.Message);
+                _logger.LogError(ex, "Failed to reply - forbidden", ex.Message);
             }
             catch (RedditControllerException ex)
             {
-                _logger.LogWarning(ex, "Failed to reply - controller error: {Message}", ex.Message);
+                _logger.LogError(ex, "Failed to reply - controller error: {Message}", ex.Message);
             }
         }
 
